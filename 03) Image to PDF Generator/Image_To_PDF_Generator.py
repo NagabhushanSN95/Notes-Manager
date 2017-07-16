54e5b1faff64c121bc9dd130e1d99dd9f1fcef1c
Nagabhushan S N <nagsn@cisco.com>
2017-07-16 09:35:35 +0530

    v0.3.2: Offset and individual page numbers can be mentioned in Bookmarks file

--- 03) Image to PDF Generator/Image_To_PDF_Generator.py
+++ 03) Image to PDF Generator/Image_To_PDF_Generator.py
@@ -6,7 +6,7 @@
 # Please execute the below line in terminal before executing the Python program
 # chmod u+x Image_To_PDF_Generator.py
 # Execute this python file as below
-# ./Image_To_PDF_Generator.py -b Bookmarks.txt -t <TITLE> -a <AUTHOR> -d ./Images/
+# ./Image_To_PDF_Generator.py -b Bookmarks.txt -md Meta-Data.txt -d ./Images/
 
 import argparse
 import os
@@ -15,6 +15,8 @@ import shutil
 META_DATA_TITLE_KEY = 'Title'
 META_DATA_AUTHOR_KEY = 'Author'
 NUM_PAGES_CONST = 'NumPages'
+OFFSET_CONST = 'Offset'
+PAGE_NOS_CONST = 'PageNos'
 
 
 def prompt(prompt_string):
@@ -40,24 +42,21 @@ def setup_args():
 def validate_inputs(args):
 	# Check if Bookmarks file exists
 	bookmarks_file_name = args.bookmarks
-	bookmarks_file = file(bookmarks_file_name, 'r')
-	if not os.path.exists(bookmarks_file):
+	if not os.path.isfile(bookmarks_file_name):
 		print("The Bookmarks file '" + bookmarks_file_name + "' doesn't exist.\n")
 		print("Exiting Program...")
 		exit()
 
 	# Check if Meta-Data file exists
 	meta_data_file_name = args.meta_data
-	meta_data_file = file(meta_data_file_name, 'r')
-	if not os.path.exists(meta_data_file):
+	if not os.path.isfile(meta_data_file_name):
 		print("The Meta-Data file '" + meta_data_file_name + "' doesn't exist.\n")
 		print("Exiting Program...")
 		exit()
 
 	# Check if Images directory exists
-	images_directory_name = args.images
-	images_directory = file(images_directory_name, 'r')
-	if not os.path.exists(images_directory):
+	images_directory_name = args.directory
+	if not os.path.isdir(images_directory_name):
 		print("The directory '" + images_directory_name + "' doesn't exist.\n")
 		print("Exiting Program...")
 		exit()
@@ -75,7 +74,7 @@ def read_inputs(args):
 	meta_data_list = meta_data_file.read().splitlines()
 	meta_data_list = [line.strip() for line in meta_data_list if line.strip()]
 	# Convert list into a Dictionary
-	meta_data = dict(map(*[iter(meta_data_list)]*2))
+	meta_data = dict(map(None, *[iter(meta_data_list)]*2))
 	meta_data_file.close()
 
 	directory = args.directory
@@ -84,19 +83,17 @@ def read_inputs(args):
 
 
 def generate_page_nos(bookmarks_data):
-	# Split input string
-	# tokens = re.split('(\n)', bookmarks_data)
-	# tokens = filter(lambda a: a != '', tokens)
-	# print bookmarks_data
-
 	num_levels = calc_num_levels(bookmarks_data)
-	# print num_levels
 
 	levels = [0] * (num_levels-1)
 	page_nos = []
 	current_level = 0
 	for i in range(0, len(bookmarks_data)):
 		current_line = bookmarks_data[i]
+		if i < len(bookmarks_data) - 1:
+			next_line = bookmarks_data[i + 1]
+		else:
+			next_line = None
 		if current_line == '{':
 			current_level += 1
 			if current_level > 1:
@@ -104,7 +101,14 @@ def generate_page_nos(bookmarks_data):
 		elif NUM_PAGES_CONST in current_line:
 			num_pages = extract_num_pages(current_line)
 			if num_pages > 0:
-				synthesize_page_nos(levels, current_level, num_pages, page_nos)
+				if (next_line is not None) and (OFFSET_CONST in next_line):
+					offset = extract_offset(next_line)
+					synthesize_serial_page_nos(levels, current_level, num_pages, page_nos, offset)
+				else:
+					synthesize_serial_page_nos(levels, current_level, num_pages, page_nos)
+		elif PAGE_NOS_CONST in current_line:
+			specific_page_nos = extract_page_nos(current_line)
+			synthesize_specific_page_nos(levels, current_level, specific_page_nos, page_nos)
 		elif current_line == '}':
 			current_level -= 1
 			reset_levels(levels, current_level)
@@ -129,12 +133,31 @@ def extract_num_pages(line):
 	return int(line.split('=')[1].strip())
 
 
-def synthesize_page_nos(levels, current_level, num_pages, page_nos):
+def extract_offset(line):
+	return int(line.split('=')[1].strip())
+
+
+def extract_page_nos(line):
+	page_nos_string = line.split('=')[1].strip()
+	page_nos = [int(page_no) for page_no in page_nos_string.split(',')]
+	return page_nos
+
+
+def synthesize_serial_page_nos(levels, current_level, num_pages, page_nos, offset=0):
 	for i in range(0, int(num_pages)):
 		page_no = ''
 		for j in range(0, current_level - 1):
 			page_no += '{:02d}'.format(levels[j]) + '.'
-		page_no += '{:02d}'.format(i + 1)
+		page_no += '{:02d}'.format(i + 1 + offset)
+		page_nos.append(page_no)
+
+
+def synthesize_specific_page_nos(levels, current_level, specific_page_nos, page_nos):
+	for specific_page_no in specific_page_nos:
+		page_no = ''
+		for j in range(0, current_level - 1):
+			page_no += '{:02d}'.format(levels[j]) + '.'
+		page_no += '{:02d}'.format(specific_page_no)
 		page_nos.append(page_no)
 
 
@@ -161,11 +184,19 @@ def generate_meta_data(meta_data_dict, bookmarks_data):
 			current_level += 1
 		elif current_line == '}':
 			current_level -= 1
-		elif NUM_PAGES_CONST not in current_line:
-			if (NUM_PAGES_CONST not in next_to_next_line) or (extract_num_pages(next_to_next_line) > 0):
-				append_bookmark(current_line, current_level, current_page_num, meta_data)
 		elif NUM_PAGES_CONST in current_line:
 			current_page_num += extract_num_pages(current_line)
+		elif OFFSET_CONST in current_line:
+			# Do Nothing
+			continue
+		elif PAGE_NOS_CONST in current_line:
+			current_page_num += len(extract_page_nos(current_line))
+		elif (NUM_PAGES_CONST not in next_to_next_line) or (extract_num_pages(next_to_next_line) > 0):
+			# If NUM_PAGES_CONST is not present in next-to-next-line, then this is the name of a chapter
+			# or something like that and hence has to be added in bookmarks
+			# If next-to-next-line has NumPages>0, then this is the lowest level, but has pages
+			# and hence this has to be added to bookmarks
+			append_bookmark(current_line, current_level, current_page_num, meta_data)
 
 	# print page_nos
 	return meta_data
@@ -189,11 +220,13 @@ def rename_files(directory, title, page_nos, bookmarks_file_name):
 		if not prompt("Warning: " + bookmarks_file_name + " lists a total of " + str(num_pages) + " pages. "
 					"But the directory '" + directory + "' has only " + str(num_files) + " files."
 					"Do you want to proceed? [Y/n]:"):
+			print "Exiting program..."
 			exit()
 	elif num_files > num_pages:
-		if not prompt("Warning: " + bookmarks_file_name + " lists a total of " + str(num_pages) + " pages. " \
-					"But the directory '" + directory + "' has " + str(num_files) + " files. " \
+		if not prompt("Warning: " + bookmarks_file_name + " lists a total of " + str(num_pages) + " pages. "
+					"But the directory '" + directory + "' has " + str(num_files) + " files. "
 					"First " + str(num_pages) + " files will be renamed. Do you want to proceed? [Y/n]:"):
+			print "Exiting program..."
 			exit()
 
 	for fileName in sorted(os.listdir('.')):
@@ -229,6 +262,7 @@ def merge_files():
 def add_bookmarks(title, meta_data):
 	cmd = "pdftk ./temp/merged.pdf dump_data > ./temp/meta_data.txt"
 	os.system(cmd)
+	# Append additional meta-data and bookmarks to existing meta-data
 	meta_data_file = file("./temp/meta_data.txt", 'a')
 	meta_data_file.writelines("%s\n" % item for item in meta_data)
 	meta_data_file.close()
@@ -249,7 +283,7 @@ def main():
 						"Do you want to remove it now? [Y/n]:")):
 			shutil.rmtree("./temp")
 		else:
-			print "Exiting program"
+			print "Exiting program..."
 			exit()
 
 	args = setup_args()
